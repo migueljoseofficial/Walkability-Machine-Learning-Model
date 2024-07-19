9c49fd6385cdaf37479eb453b6226b271229dc80
Miguel Jose Maninang <miguel_maninang@yahoo.com>
2024-07-19 14:07:51 -0400

    some new stuff in sifting

--- sifting.py
+++ sifting.py
@@ -1,92 +1,75 @@
 
 import pandas as pd
+import numpy as np
 from typing import List
 
+def find_matching_street(lat, lon, street_data):
+    for _, street in street_data.iterrows():
+        if (street_data['left_bound_long'] <= lon <= street_data['right_bound_long'] and
+            street_data['down_bound_lat'] <= lat <= street_data['up_bound_lat']):
+            return street
+    return None
 
-##function to add the columns to a specified row.
-# def add_columns(heart_data: pd.DataFrame, street_data: pd.DataFrame, street_columns: List[str], index):
-#     for column in street_columns:
-#         heart_data.at[index, column] = street_data[column].values[0]
-import numpy as np
 
-def remove_irrelevant_rows(
-    heart_data: pd.DataFrame,
-    latitude_min: float = 41.677476,
-    latitude_max: float = 41.679436,
-    longitude_min: float = -86.253347,
-    longitude_max: float = -86.243803
-) -> pd.DataFrame:
-    filtered_data = heart_data[
-        (heart_data['Latitude'].between(latitude_min, latitude_max)) &
-        (heart_data['Longitude'].between(longitude_min, longitude_max))
-    ]
-    return filtered_data
-
+#function to add the columns to a specified row.
+def add_columns(heart_data: pd.DataFrame, street_data: pd.DataFrame, street_columns: List[str], index):
+    for column in street_columns:
+        heart_data.at[index, column] = street_data[column].values[0]
 
 
 ##initalizes the data sets into pd dataframes
-heart_data = './dataset/heartrate_data.csv'
-street_data = './dataset/prelim_data.csv'
+heart_data = './dataset/filtered_heart_data.csv'
+street_data = './dataset/second_data.csv'
 
 
-dirty_heart_file = pd.read_csv(heart_data)
+heart_file = pd.read_csv(heart_data)
 street_file = pd.read_csv(street_data)
 
-
-
-
-
-dirty_heart_file['Latitude'] = pd.to_numeric(dirty_heart_file['Latitude'], errors='coerce')
-dirty_heart_file['Longitude'] = pd.to_numeric(dirty_heart_file['Longitude'], errors='coerce')
-##cleans file of irrelevant data
-
-
-heart_file = remove_irrelevant_rows(
-    dirty_heart_file, 
-    latitude_min= 41.677476,
-    latitude_max = 41.679436,
-    longitude_min= -86.243803,
-    longitude_max = -86.253347)
-
-print(heart_file)
-
-
-heart_file.to_csv('filtered_heart_data.csv', index=False)
 street_columns = [
-    'streetname', 'num_of_uplifts', 'maintenance_suggestion', 'repair_priority', 'surface_type',
-    'length_ft', 'area_sy', 'sci_2023', 'sw_id', 'avg_width', 'num_of_distresses', 'bufferwidth_avg',
-    'num_crack_minor', 'num_crack_moderate', 'num_crack_severe', 'num_grass_present', 
-    'num_surface_distortion_minor', 'num_surface_distortion_mod', 'num_surface_distortion_severe',
-    'num_uplift_general_minor', 'num_uplift_general_moderate', 'num_uplift_general_severe',
-    'num_uplift_tree_minor', 'num_uplift_tree_moderate', 'num_uplift_tree_severe',
-    'num_utility_hazard_present', 'avg_stress_lvl_Dummy'
+    'streetname', 'fromstreetname', 'tostreetname', 'sci', 'num_of_uplifts',
+    'maintenance_suggestion', 'estimated_cost', 'repair_priority', 'functional_class',
+    'functional_class_id', 'surface_type', 'survey_date', 'width_ft', 'length_ft',
+    'area_sy', 'sci_2023', 'sw_id', 'begin_width', 'end_width', 'avg_width',
+    'nu_of_distresses', 'num_crack_minor', 'num_crack_moderate', 'num_crack_severe',
+    'num_grass_present', 'num_surface_distortion_minor', 'num_surface_distortion_mod',
+    'num_surface_distortion_severe', 'num_uplift_general_minor', 'num_uplift_general_moderate',
+    'num_uplift_general_severe', 'num_uplift_tree_minor', 'num_uplift_tree_moderate',
+    'num_uplift_tree_severe', 'num_utility_hazard_present', 'left_bound_long',
+    'right_bound_long', 'down_bound_lat', 'up_bound_lat'
 ]
 
 
-##initalizes new columns within the heart data
-# for column in street_columns:
-#     heart_file[column] = None
+#initalizes new columns within the heart data
+# Initialize new columns within the heart data with appropriate dtypes
+for column in street_columns:
+    if column in street_file.select_dtypes(include=['object']).columns:
+        heart_file[column] = pd.Series(dtype='object')
+    else:
+        heart_file[column] = pd.Series(dtype='float64')
 
-# print(street_file['streetname'].values)
-## goes through every row of the heart data 
-# for index, row in heart_data.iterrows():
-#     longitude = row['longitude']
-#     latitue = row['latitude']
-
-#     if longitude and latitude matches a :
-#         pass
+# goes through every row of the heart data 
+for index, row in heart_file.iterrows():
+    longitude = row['Longitude']
+    latitude = row['Latitude']
+    matching_street = find_matching_street(latitude, longitude, street_file)
+    
+    if matching_street is not None:
+        for column in street_columns:
+            heart_file.at[index, column] = matching_street[column]
         
        
 
 
         
+# Calculate average stress level for each street
+heart_file['avg_stress_lvl_Dummy'] = heart_file.groupby('streetname')['Stress score'].transform('mean')
+
+# Save the updated data to a new CSV file
+heart_file.to_csv('updated_file.csv', index=False)
 
 
-# heart_data.to_csv('updated_file.csv', index=False)
 
-
-#3
-## if latitude between lattitude_min and Latitude_max
-    ##if between longtidue_min and longtiude_max
-        ##
+# if latitude between lattitude_min and Latitude_max
+    #if between longtidue_min and longtiude_max
+        #
 
