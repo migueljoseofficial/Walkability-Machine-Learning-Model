1aebb6bf9d53fe97bd80bde03706142dbbf16e4b
Miguel Jose Maninang <miguel_maninang@yahoo.com>
2024-07-19 13:29:27 -0400

    added filtered heart data

--- sifting.py
+++ sifting.py
@@ -7,19 +7,18 @@ from typing import List
 # def add_columns(heart_data: pd.DataFrame, street_data: pd.DataFrame, street_columns: List[str], index):
 #     for column in street_columns:
 #         heart_data.at[index, column] = street_data[column].values[0]
+import numpy as np
 
 def remove_irrelevant_rows(
     heart_data: pd.DataFrame,
     latitude_min: float = 41.677476,
     latitude_max: float = 41.679436,
-    longitude_min: float = -86.243803,
-    longitude_max: float = -86.253347
+    longitude_min: float = -86.253347,
+    longitude_max: float = -86.243803
 ) -> pd.DataFrame:
     filtered_data = heart_data[
-        (heart_data['Latitude'] >= latitude_min) &
-        (heart_data['Latitude'] <= latitude_max) &
-        (heart_data['Longitude'] >= longitude_min) &
-        (heart_data['Longitude'] <= longitude_max)
+        (heart_data['Latitude'].between(latitude_min, latitude_max)) &
+        (heart_data['Longitude'].between(longitude_min, longitude_max))
     ]
     return filtered_data
 
@@ -32,13 +31,26 @@ street_data = './dataset/prelim_data.csv'
 
 dirty_heart_file = pd.read_csv(heart_data)
 street_file = pd.read_csv(street_data)
+
+
+
+
+
 dirty_heart_file['Latitude'] = pd.to_numeric(dirty_heart_file['Latitude'], errors='coerce')
 dirty_heart_file['Longitude'] = pd.to_numeric(dirty_heart_file['Longitude'], errors='coerce')
 ##cleans file of irrelevant data
-heart_file = remove_irrelevant_rows(dirty_heart_file, latitude_min= 41.677476,
+
+
+heart_file = remove_irrelevant_rows(
+    dirty_heart_file, 
+    latitude_min= 41.677476,
     latitude_max = 41.679436,
     longitude_min= -86.243803,
     longitude_max = -86.253347)
+
+print(heart_file)
+
+
 heart_file.to_csv('filtered_heart_data.csv', index=False)
 street_columns = [
     'streetname', 'num_of_uplifts', 'maintenance_suggestion', 'repair_priority', 'surface_type',
